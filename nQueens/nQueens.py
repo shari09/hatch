2887a7148eae45e7a1a6f22cee836e94f36a7478
Shari Sun <shari09cxxcr@gmail.com>
2019-11-08 13:48:37 -0500

    n queens problem

--- nQueens/nQueens.py
+++ nQueens/nQueens.py
@@ -2,7 +2,7 @@ def isValid(board, row, col):
   for i in range(len(board)):
     for j in range(len(board)):
       if board[i][j] == 1:
-        if i == row or j == col or abs(row-i) == abs(col-i):
+        if i == row or j == col or abs(row-i) == abs(col-j):
           return False
   return True
 
@@ -11,9 +11,22 @@ def display(board):
     for col in row:
       print(col, end=' ')
     print()
+  print()
 
-def solve(board, row, col):
-  
+def solve(board, row, col, n):
+  if n == 0:
+    display(board)
+    return 1
+  if col == len(board):
+    return 0
+
+  numSolutions = 0
+  for i in range(len(board)):
+    if isValid(board, row, i):
+      board[row][i] = 1
+      numSolutions += solve(board, row+1, 0, n-1)
+      board[row][i] = 0
+  return numSolutions
 
 def main():
   
@@ -23,7 +36,8 @@ def main():
   for i in range(size):
     board[i] = [0] * size
 
-  display(board)
+  numSolutions = solve(board, 0, 0, size)
+  print('Number of solutions:', numSolutions)
 
 if __name__ == '__main__':
   main()
