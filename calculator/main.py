38eba150739c5eae602e741d0a93d7b82b92f455
Shari Sun <shari09cxxcr@gmail.com>
2019-11-08 19:44:58 -0500

    minimistic(is this a word) calculator

--- calculator/main.py
+++ calculator/main.py
@@ -1,17 +1,128 @@
+#the most basic calculator made with Turtles
+#lots of functions can be added
 import turtle;
+
 calculator = turtle.Turtle()
 screen = turtle.Screen()
 turtle.tracer(0, 0)
 turtle.penup()
+SIZE = 50
 
-buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9, '%', 0, '.']
-def test(x, y):
-    print(x, y)
-style = ('Arial', 30, 'normal')
+class Screen:
+  def __init__(self, x, y, width, height, numLines):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.fontSize = 10
+    self.numLines = numLines
+    self.lineHeight = self.height//self.numLines
+    self.height = self.lineHeight*self.numLines
+    self.lines = ['']*self.numLines
+
+  def reset(self):
+    self.fontSize = 10
+    self.lines = ['']*self.numLines
+
+  def add(self, value):
+    
+    if len(self.lines[0]) * self.fontSize >= self.width-self.fontSize \
+       or len(self.lines[1]) * self.fontSize >= self.width-self.fontSize:
+      if (self.fontSize > 5):
+        self.fontSize -= 1
+      else:
+        return
+    
+    if len(self.lines[1]) == 0 and value.isdigit():
+      self.lines[0] = ''
+    elif len(self.lines[1]) == 0 and not value.isdigit():
+      self.lines[1] = self.lines[0]
+
+    self.lines[0] += value
+    if value.isdigit():
+      self.lines[1] = str(eval(self.lines[0]))
+
+
+    
+  def evaluate(self):
+    self.lines[0] = self.lines[1]
+    self.lines[1] = ''
+
+
+  def display(self):
+    turtle.goto(self.x, self.y)
+    turtle.pendown()
+    for i in range(2):
+      turtle.forward(self.width)
+      turtle.left(90)
+      turtle.forward(self.height)
+      turtle.left(90)
+    turtle.penup()
+    for lineIdx in range(len(self.lines)):
+      lineLen = len(self.lines[lineIdx])
+      for charIdx in range(lineLen):
+        turtle.goto(self.x+self.width - self.fontSize - self.fontSize*(lineLen-charIdx), self.y + self.numLines*self.lineHeight-(lineIdx+1)*self.lineHeight)
+        turtle.write(self.lines[lineIdx][charIdx], font=('Arial', self.fontSize, 'normal'))
+
+class Button:
+  def __init__(self, x, y, value):
+    self.x = x
+    self.y = y
+    self.value = str(value)
+    self.size = SIZE
+
+  def clicked(self, xPos, yPos):
+    if self.x < xPos < self.x + self.size \
+        and self.y < yPos < self. y + self.size:
+      return True
+
+  def display(self):
+    turtle.goto(self.x, self.y)
+    turtle.pendown()
+    for i in range(4):
+      turtle.forward(self.size)
+      turtle.left(90)
+    turtle.penup()
+    turtle.goto(self.x+self.size/2, self.y)
+    turtle.write(self.value, align='center', font=('Arial', 30, 'normal'))
+
+def addToScreen(value, screen):
+  if value == '=':
+    screen.evaluate()
+  else:
+    screen.add(value)
+
+
+buttons = []
+for i in range(3):
+  for j in range(1, 4):
+    buttons.append(Button(i*SIZE, j*SIZE, i+(j-1)*3+1))
+buttons.append(Button(0, 0, 0))
+buttons.append(Button(SIZE, 0, '.'))
+buttons.append(Button(2*SIZE, 0, '='))
+buttons.append(Button(3*SIZE, 0, '/'))
+buttons.append(Button(3*SIZE, SIZE, '*'))
+buttons.append(Button(3*SIZE, 2*SIZE, '-'))
+buttons.append(Button(3*SIZE, 3*SIZE, '+'))
+buttons.append(Button(-SIZE, 3*SIZE, 'C'))
+calcScreen = Screen(0, 4*SIZE, 4*SIZE, SIZE, 2)
+
+def check(x, y):
+  turtle.clear()
+  for button in buttons:
+    if button.clicked(x, y):
+      if button.value == 'C':
+        calcScreen.reset()
+      else:
+        addToScreen(button.value, calcScreen)
+  displayAll()
+
+def displayAll():
+  for button in buttons:
+      button.display()
+  calcScreen.display()
+
+displayAll()
 while True:
-    screen.onclick(test)
-    turtle.write(1, font=style)
-    turtle.goto(50, 0)
-    turtle.write(2, font=style)
-
-    turtle.update()
+  screen.onclick(check)
+  turtle.update()
